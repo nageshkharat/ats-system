1a1bec0ba7172c132588507470acaf303be73b76
Nagesh Kharat <130240484+nageshkharat@users.noreply.github.com>
2025-07-10 23:40:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const pdfParse = require('pdf-parse');
 const bodyParser = require('body-parser');
 const path = require('path');
-const atsScoring = require('./utils/atsScoring');
+const atsScoring = require('./utils/atsScoring'); 
 const fs = require('fs');
  
 const app = express();
