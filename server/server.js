c005a2a15971d5564c5fbadc5981062539a61cbb
Nagesh Kharat <130240484+nageshkharat@users.noreply.github.com>
2025-10-14 23:52:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-const multer = require('multer');  
+const multer = require('multer');   
 const pdfParse = require('pdf-parse');          
 const bodyParser = require('body-parser');              
 const path = require('path');           
